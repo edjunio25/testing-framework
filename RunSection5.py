4878d161ecfcf034606aa34aa8c216da75388168
edjunio25 <edson.jbp@hotmail.com>
2025-05-05 23:44:05 -0300

    5. Classe TestSuite

---
+++ RunSection5.py
@@ -0,0 +1,24 @@
+from TestCase import TestCase
+from TestSuite import TestSuite
+from TestResult import TestResult
+from TestCaseTest import TestCaseTest
+from TestSuiteTest import TestSuiteTest
+
+result = TestResult()
+suite = TestSuite()
+
+suite.add_test(TestCaseTest('test_result_success_run'))
+suite.add_test(TestCaseTest('test_result_failure_run'))
+suite.add_test(TestCaseTest('test_result_error_run'))
+suite.add_test(TestCaseTest('test_result_multiple_run'))
+suite.add_test(TestCaseTest('test_was_set_up'))
+suite.add_test(TestCaseTest('test_was_run'))
+suite.add_test(TestCaseTest('test_was_tear_down'))
+suite.add_test(TestCaseTest('test_template_method'))
+
+suite.add_test(TestSuiteTest('test_suite_size'))
+suite.add_test(TestSuiteTest('test_suite_success_run'))
+suite.add_test(TestSuiteTest('test_suite_multiple_run'))
+
+suite.run(result)
+print(result.summary())
