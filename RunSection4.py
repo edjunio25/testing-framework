faf30a3b65ff988824d620e5d01ee6d71bd2b422
edjunio25 <edson.jbp@hotmail.com>
2025-05-05 22:14:04 -0300

    4. Testando TestCase

---
+++ RunSection4.py
@@ -0,0 +1,30 @@
+from TestCase import TestCase
+from TestResult import TestResult
+from TestCaseTest import TestCaseTest
+result = TestResult()
+
+test = TestCaseTest('test_result_success_run')
+test.run(result)
+
+test = TestCaseTest('test_result_failure_run')
+test.run(result)
+
+test = TestCaseTest('test_result_error_run')
+test.run(result)
+
+test = TestCaseTest('test_result_multiple_run')
+test.run(result)
+
+test = TestCaseTest('test_was_set_up')
+test.run(result)
+
+test = TestCaseTest('test_was_run')
+test.run(result)
+
+test = TestCaseTest('test_was_tear_down')
+test.run(result)
+
+test = TestCaseTest('test_template_method')
+test.run(result)
+
+print(result.summary())
